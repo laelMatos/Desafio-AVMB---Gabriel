b5b2e1d3cd389188bb6a4be668d52b447a31909f
Lael Santos de Matos <cglael@gmail.com>
2022-11-18 15:05:14 -0300

    Task1 - documentação da função QuantImpares

--- Task1.js
+++ Task1.js
@@ -1,3 +1,8 @@
+/**
+ ** Conta quantos numeros impares existem na lista.
+ * @param {number[]} list Lista de numeros a serem verificados
+ * @returns {number} Quantidade de numeros impares
+ */
 function QuantImpares(list){
     try{
         let result=0;
